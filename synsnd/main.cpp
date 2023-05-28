1dc3241b473ee29981ec524755db6130beddf7e8
“Stephen <stephen.hazel@gmail.com>
2023-05-27 17:50:32 -0700

    startin to hook pcheetah to syn - first will be slash ta _

--- synsnd/main.cpp
+++ synsnd/main.cpp
@@ -25,9 +25,11 @@ bool DoDir (void *ptr, char dfx, char *fn)
 { ubyte nsl = 0, ln = StrLn (fn);
   char *p;
   TStr  s, t, bnk, kit;
-   if (dfx == 'd') {
+//DBG("got dfx=`c fn=`s", dfx, fn);
+   if ((dfx == 'd') && (ln > StrLn (DirSyn))) {
       p = & fn [StrLn (DirSyn)+1];     // past .../device/syn/
       while ((p = StrCh (p, '/')) != nullptr)  {nsl++;   p++;}
+//DBG("nsl=`d", nsl);
       if ( nsl &&                                // not raw bank
            (ln > 5) &&                           // long enough fer anything
            StrCm (& fn [ln-5], CC("/drum")) &&   // not raw drum dir
@@ -37,7 +39,9 @@ bool DoDir (void *ptr, char dfx, char *fn)
         ColSep c (t, 1, '/');
          StrCp (bnk, c.Col [0]);
          StrCp (s,   c.Col [1]);
-         StrFmt (Snd [NSnd++], "`s_`s", s, bnk);
+         StrFmt (Snd [NSnd], "`s_`s", s, bnk);
+DBG("   snd=`s n=`d", Snd[NSnd], NSnd);
+         NSnd++;
       }
    }
    return false;  // neva stop
@@ -51,25 +55,32 @@ int SndCmp (void *p1, void *p2)
   TStr  b1, b2;
    StrCp (b1, (char *)p1);   s1 = & b1 [0];
    StrCp (b2, (char *)p2);   s2 = & b2 [0];
+//DBG("s1=`s s2=`s", s1, s2);
    d1 = MemCm (s1, CC("drum/"), 5) ? 1 : 0;      // 0=drum vs 1=melo
    d2 = MemCm (s2, CC("drum/"), 5) ? 1 : 0;
    if (d1 - d2)  return d1-d2;
 
+//DBG("drum same");
    if (d1) {                           // melodic sounds - pretty ez
+//DBG("melo");
       for (g1 = 0;  g1 < BITS (MGrp);  g1++)
          if (! MemCm (s1, MGrp [g1], StrLn (MGrp [g1])))  break;
       for (g2 = 0;  g2 < BITS (MGrp);  g2++)
          if (! MemCm (s2, MGrp [g2], StrLn (MGrp [g2])))  break;
       if (g1 - g2)  return g1-g2;                // grp order
+//DBG("melo grp same");
       return StrCm (s1, s2);                     // then strcm snd,bnk
+//DBG("melo cmp");
    }
                                        // left w drum/*
    g1 = MemCm (s1, CC("drum/x_"), 6) ? 0 : 1;    // 1=x_999_b_k, 0=grp_snd_b_k
    g2 = MemCm (s2, CC("drum/x_"), 6) ? 0 : 1;
    if (g1 - g2)  return g1-g2;
+//DBG("drum/x_ same");
 
-   if (g1 == 0)  return StrCm (s1, s2);          // ez
+   if (g1 == 1)  return StrCm (s1, s2);          // ez
 
+//DBG(" not x_ w just cmp");
 // cmp MDrum pos by drnt pos in MDrum (sorts by dgrp,drnt)
    for (n1 = 0;  n1 < NMDrum;  n1++)
       if (! MemCm (& s1 [10], MDrum [n1].sym, 4))  break;
@@ -77,7 +88,8 @@ int SndCmp (void *p1, void *p2)
       if (! MemCm (& s2 [10], MDrum [n2].sym, 4))  break;
    if (n1 - n2)  return n1-n2;         // diff drum/dgrp_dsnd
 
-   return StrCm (& s1 [o+4], & s2 [o+4]);        // else by bnk,kit
+//DBG("drm order same so cmp");
+   return StrCm (s1, s2);              // else by kit,bnk
 }
 
 
