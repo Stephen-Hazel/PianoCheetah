e2e005c6a26d84269a76a4ff3e1842298729c331
Stephen Hazel <stephen.hazel@gmail.com>
2026-01-03 09:51:27 -0800

    ok i think fpak is good

--- b.sh
+++ b.sh
@@ -1,9 +1,8 @@
 #!/bin/php
 <?php
 // b - build with flatpak-builder n stuff
-// args:   o   build only - for release to flathub
-//         c   wipe config dir - for full reinstall
-//         d   build with debugging so ya can gdb inside it
+// args:  c  wipe config dir - for full reinstall
+//        d  build with debugging so ya can gdb inside it
    $arg = '';   if ($argc > 1)  $arg = $argv [1];
 
 // app triplet n fpak cmds
@@ -25,7 +24,7 @@ echo "...uninstall old one\n";
       where
       bt full
 */    system ("mkdir _build");
-      system ("$fb --user --force-clean --install _build _manif.dbg", $rc);
+      system ("$fb --user --force-clean --install _build fpak.dbg", $rc);
       system ("$f install --reinstall --user --assumeyes ".
               "/home/sh/src/pianocheetah/.$fb/cache $app");
       system ("$f install --reinstall --user --assumeyes ".
@@ -37,22 +36,11 @@ echo "...uninstall old one\n";
    system ("mkdir _build");
 
 echo "...compilin n installin\n";
-   system ("$fb --user --install _build _manif", $rc);
-   if (($rc != 0) || ($arg == 'o'))  exit;
-                                       // build error :(  or build only
-/*
-echo "...add repo\n";
-   system ("$f remote-add    _repo --no-gpg-verify");
-
-echo "...install it\n";
-   system ("$f install       _repo $app --noninteractive");
-
-echo "...remove repo\n";
-   system ("$f remote-delete _repo --force");
-*/
+   system ("$fb --user --install _build fpak", $rc);
+   if ($rc != 0)  exit;                // build error :(
 
 echo "...cleanup\n";
-#  system ("rm -fr _build .$fb");     // cleanup
+   system ("rm -fr _build .$fb");
    if ($arg != 'c')  exit;
 
 // cleanup .var/app/ dir for full reset (wipe config files etc)
